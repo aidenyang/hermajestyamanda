5637faadba85aa6482887047eb8f40ccc73bc892
Alison Yang <alisonlyang@gmail.com>
2013-08-16 19:07:22 -0400

    added some content

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -3,11 +3,15 @@
 angular.module('hermajestyamandaApp', [])
   .config(function ($routeProvider) {
     $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
+      .when('/hermajesty', {
+        templateUrl: 'views/hermajesty.html',
+        controller: 'main-control'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'about-control'
       })
       .otherwise({
-        redirectTo: '/'
+        redirectTo: '/hermajesty'
       });
   });
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,10 +1,12 @@
 'use strict';
 
 angular.module('hermajestyamandaApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('main-control', function ($scope, $http) {
+  	$http({
+		method: 'GET'
+		url: 'https://www.googleapis.com/customsearch/v1?key=AIzaSyC5CN_SAp2quqY5VpExP7-7X3vomVMc8F4&cx=006334694207456473384:dpedvhja9em&searchType=image&q=queen+elizabeth&alt=json'
+		})
+	.success(function(data){
+		console.log(data);
+	})
   });
