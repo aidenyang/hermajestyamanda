a9af9caad2d35a74c35033aa158ec254cfe0c1d1
Alison Yang <alisonlyang@gmail.com>
2013-08-15 23:25:57 -0400

    yeoman

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('hermajestyamandaApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
