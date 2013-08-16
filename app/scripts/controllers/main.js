01996af64b2189ce08bc39223c2ab3755b700368
Alison Yang <alisonlyang@gmail.com>
2013-08-16 19:30:14 -0400

    fixing .git

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,7 +3,7 @@
 angular.module('hermajestyamandaApp')
   .controller('main-control', function ($scope, $http) {
   	$http({
-		method: 'GET'
+		method: 'GET',
 		url: 'https://www.googleapis.com/customsearch/v1?key=AIzaSyC5CN_SAp2quqY5VpExP7-7X3vomVMc8F4&cx=006334694207456473384:dpedvhja9em&searchType=image&q=queen+elizabeth&alt=json'
 		})
 	.success(function(data){
