4b9c5757af0c9448b52091835584f7b45fbe0495
Alison Yang <alisonlyang@gmail.com>
2013-08-18 13:46:06 -0400

    new pics & some formatting

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -5,7 +5,7 @@ angular.module('hermajestyamandaApp')
 .controller('main-control', function ($scope, $http) {
 	$scope.notReady = true;
 	$scope.bynestweet = "loading...";
-	$scope.randompicindex = Math.floor((Math.random()*10)+0);
+	$scope.randompicindex = Math.floor((Math.random()*27)+1);
 
 	var params= {
 		screen_name:"amandabynes",
@@ -33,7 +33,7 @@ angular.module('hermajestyamandaApp')
 			for (var i=0; i<$scope.timelinelength; i++){
 				$scope.tweets.push(data[i].text);	
 			}
-			$scope.randompicindex = Math.floor((Math.random()*10)+0);
+			$scope.randompicindex = Math.floor((Math.random()*27)+1);
 			$scope.randomtweetindex = Math.floor((Math.random()*$scope.timelinelength)+0);
 			$scope.bynestweet = $scope.tweets[$scope.randomtweetindex];
 			$scope.notReady = false;
@@ -43,7 +43,7 @@ angular.module('hermajestyamandaApp')
 		true
 		);
 	$scope.newpair = function(){
-		$scope.randompicindex = Math.floor((Math.random()*10)+0);
+		$scope.randompicindex = Math.floor((Math.random()*27)+1);
 		$scope.randomtweetindex = Math.floor((Math.random()*$scope.timelinelength)+0);
 		$scope.bynestweet = $scope.bynestweet = $scope.tweets[$scope.randomtweetindex];
 
