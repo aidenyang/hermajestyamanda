6e1bd174b86ab92b8ca04329d5942980540efcc2
Alison Yang <alisonlyang@gmail.com>
2013-08-18 12:42:25 -0400

    most functionality added

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,11 +3,52 @@
 angular.module('hermajestyamandaApp')
 
 .controller('main-control', function ($scope, $http) {
-	var promise = $http.jsonp('https://www.googleapis.com/customsearch/v1?key=AIzaSyC5CN_SAp2quqY5VpExP7-7X3vomVMc8F4&cx=006334694207456473384:dpedvhja9em&q=queen+elizabeth&alt=json&searchtype=image&callback=JSON_CALLBACK&num=1');
-	promise.success(function(data){
-		$scope.majestyurl = data.items[0].pagemap.cse_image[0];		
-		console.log($scope.majestyurl);
-	})
+	$scope.notReady = true;
+	$scope.bynestweet = "loading...";
+	$scope.randompicindex = Math.floor((Math.random()*10)+0);
+
+	var params= {
+		screen_name:"amandabynes",
+		include_rts: "false",
+		count: "200"
+	};
+
+	var cb = new Codebird;
+	cb.setConsumerKey("7SPZ0zOMFBzGh6zgQf3Jg", "guB52d3iXKGLxR8wt9AVt4TyNujp9UvkHY64yRM");
+	
+	cb.__call(
+		"oauth2_token",
+		{},
+		function (reply) {
+			var bearer_token = reply.access_token;
+			cb.setBearerToken(bearer_token);
+		}
+		);	
+	cb.__call(
+		"statuses_userTimeline",
+		params,
+		function(data){
+			$scope.tweets = [];
+			$scope.timelinelength = data.length;
+			for (var i=0; i<$scope.timelinelength; i++){
+				$scope.tweets.push(data[i].text);	
+			}
+			$scope.randompicindex = Math.floor((Math.random()*10)+0);
+			$scope.randomtweetindex = Math.floor((Math.random()*$scope.timelinelength)+0);
+			$scope.bynestweet = $scope.tweets[$scope.randomtweetindex];
+			$scope.notReady = false;
+			$scope.$apply();
+			
+		}, 
+		true
+		);
+	$scope.newpair = function(){
+		$scope.randompicindex = Math.floor((Math.random()*10)+0);
+		$scope.randomtweetindex = Math.floor((Math.random()*$scope.timelinelength)+0);
+		$scope.bynestweet = $scope.bynestweet = $scope.tweets[$scope.randomtweetindex];
+
+	}
+
 
 });
 
